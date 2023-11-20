1cdf96988623b9d75044d827abf891fbec593dae
Shaduruk <jide@tidjani-serpos.com>
2023-11-20 04:26:02 -0500

    code application config_file controllers models routes and index

---
+++ models/Thought.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+const Reaction = require("./Reaction");
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [Reaction],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `responses` that gets the date of creation by the user for each thought
+thoughtSchema
+  .virtual("getCreationDate")
+  // Getter
+  .get(function () {
+    return this.createdAt.toLocaleDateString();
+  });
+
+thoughtSchema
+  .virtual("reactionCount")
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our Thought model
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
